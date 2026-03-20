db62022051afce682d126ae82af4c8642375833d
hogiljung <horlf29@gmail.com>
2026-03-20 17:59:22 +0900

    fix: init data 없어진거 수정

--- backend/src/main/java/com/team12/backend/global/initData/BaseInitData.java
+++ backend/src/main/java/com/team12/backend/global/initData/BaseInitData.java
@@ -2,6 +2,9 @@ package com.team12.backend.global.initData;
 
 import com.team12.backend.domain.customer.entity.Customer;
 import com.team12.backend.domain.customer.repository.CustomerRepository;
+import com.team12.backend.domain.order.entity.Order;
+import com.team12.backend.domain.order.entity.OrderItem;
+import com.team12.backend.domain.order.repository.OrderRepository;
 import com.team12.backend.domain.product.entity.Product;
 import com.team12.backend.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,8 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
 @Configuration
 @Profile("dev")
 @RequiredArgsConstructor
@@ -23,6 +28,7 @@ public class BaseInitData {
     private BaseInitData self;
     private final CustomerRepository customerRepository;
     private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
     @Bean
     ApplicationRunner initDataRunner() {
@@ -60,5 +66,15 @@ public class BaseInitData {
             ));
         }
 
+        if (orderRepository.count() == 0) {
+            Customer customer = customerRepository.findById(1).get();
+            Product product = productRepository.findById(1).get();
+            Order order = new Order(
+                    "address", "postcode", false,
+                    0, customer, new ArrayList<>());
+            OrderItem orderItem = new OrderItem(1, product.getPrice(), order, product);
+            order.addOrderItem(orderItem);
+            orderRepository.save(order);
+        }
     }
 }
