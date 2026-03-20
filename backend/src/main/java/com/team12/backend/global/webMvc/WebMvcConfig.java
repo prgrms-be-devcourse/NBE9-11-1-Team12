67b1da0764b6eafa5cfc4759e5fd4c74d690b5fb
hogiljung <horlf29@gmail.com>
2026-03-20 10:07:11 +0900

    Update CORS mapping to allow all origins and enable API documentation

--- backend/src/main/java/com/team12/backend/global/webMvc/WebMvcConfig.java
+++ backend/src/main/java/com/team12/backend/global/webMvc/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
